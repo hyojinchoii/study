4806dcfe02caf1b97dd3eab9f12c8b6baafb151d
study2 <mnmn0905@naver.com>
2022-08-14 17:34:10 +0900

    Changes

--- lecture/Week5/메모 만들기.py
+++ lecture/Week5/메모 만들기.py
@@ -18,3 +18,24 @@ class memolist:
     def insertmemo(self,title,content):
         self.memo_list.append(memo(title,content))
 
+type = input('메모입력: 1, 종료 : 0')
+while True:
+    if type == '1':
+        title = input('제목을 입력하세요 \n')
+        content = input('내용을 입력하세요\n')
+        memo1 = memolist()
+        memo1.insertmemo(title,content)
+        yn = input('다음 메모를 입력하시겠습니까? y/n')
+        if yn == 'y' or yn == 'Y':
+            continue
+        elif yn == 'n' or yn == 'N':
+            break
+        else:
+            print('잘 못된 값입니다')
+            break
+    elif type == 0 :
+        break
+
+
+
+
